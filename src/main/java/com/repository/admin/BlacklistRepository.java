0b08b3605242446ca64b1d4ced10f8a9dfe781eb
장은호 <bcb119@naver.com>
2025-04-28 17:11:16 +0900

    네번dsfffffsdasdads

--- src/main/java/com/repository/admin/BlacklistRepository.java
+++ src/main/java/com/repository/admin/BlacklistRepository.java
@@ -3,5 +3,8 @@ package com.repository.admin;
 import com.entity.admin.Blacklist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BlacklistRepository extends JpaRepository<Blacklist,Long> {
+    Optional<Blacklist> findByUserId(Long id);
 }
