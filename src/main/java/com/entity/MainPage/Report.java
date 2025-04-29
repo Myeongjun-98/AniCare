3928d6fd19d7c2f3c5a970c627f6a4d04be2e9b9
eunjin466 <dmswls10917@naver.com>
2025-04-29 10:52:02 +0900

    1234

--- src/main/java/com/entity/MainPage/Report.java
+++ src/main/java/com/entity/MainPage/Report.java
@@ -1,4 +1,4 @@
-package com.entity.entity.MainPage;
+package com.entity.MainPage;
 
 import com.entity.community.Board;
 import jakarta.persistence.*;
--- src/main/java/com/entity/MainPage/User.java
+++ src/main/java/com/entity/MainPage/User.java
@@ -1,4 +1,4 @@
-package com.entity.entity.MainPage;
+package com.entity.MainPage;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
