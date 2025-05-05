140c6661261dbe0d86cfac116460b39c0b769f78
Gyam614 <hat0614@gmail.com>
2025-05-06 01:10:06 +0900

    20250505 ✔ boardWrite 구현  - boardWrite post 요청 매핑 점검하기

---
+++ src/main/java/com/config/WebMbvConfig.java
@@ -0,0 +1,19 @@
+package com.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMbvConfig implements WebMvcConfigurer {
+
+    @Value("${uploadPath}")
+    String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/anicareFile/**")
+                .addResourceLocations(uploadPath);
+    }
+
+
+}
