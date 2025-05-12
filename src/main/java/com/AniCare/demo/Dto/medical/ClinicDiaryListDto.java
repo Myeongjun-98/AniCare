2d1bbd10e2f0625001c0828d492600563021f676
Myeongjun-98 <anju304@naver.com>
2025-05-12 09:29:45 +0900

    0512 작업중

--- src/main/java/com/AniCare/demo/Dto/medical/ClinicDiaryListDto.java
+++ src/main/java/com/AniCare/demo/Dto/medical/ClinicDiaryListDto.java
@@ -1,23 +1,36 @@
 package com.AniCare.demo.DTO.medical;
 
 import com.AniCare.demo.constant.medical.PetStatus;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 @Setter
 public class ClinicDiaryListDto {
 
     private Long clinicDiaryId; // 진료수첩 식별용 아이디
-    private LocalDate boardWriteDate;   // LocalDateTime -> LocalDate , 작성일
+    private LocalDate recordDate;   // LocalDateTime -> LocalDate , 날짜(설정일)
     private String boardTitle;  // 게시글 제목
-    private int boardHit;       // 게시글 조회수
+    private Integer boardHit;       // 게시글 조회수
     private PetStatus status;   // 반려동물 병 상태
 
+    // JPQL Projection 용 생성자: 2번째 파라미터는 LocalDateTime
+    public ClinicDiaryListDto(
+            Long clinicDiaryId,
+            LocalDateTime fullDateTime,
+            String boardTitle,
+            Integer boardHit,
+            PetStatus status
+    ) {
+        this.clinicDiaryId = clinicDiaryId;
+        this.recordDate = fullDateTime.toLocalDate();  // 여기서 Date 변환
+        this.boardTitle = boardTitle;
+        this.boardHit = boardHit;
+        this.status = status;
+    }
 }
