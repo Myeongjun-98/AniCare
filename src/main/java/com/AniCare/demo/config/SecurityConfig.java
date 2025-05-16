f3afea861f6017b711c5790f9c539354b62c4d74
khj <hato614@gmail.com>
2025-05-16 17:21:19 +0900

    Com

--- src/main/java/com/AniCare/demo/config/SecurityConfig.java
+++ src/main/java/com/AniCare/demo/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/anicare", "/anicare/**",
                                 "/mainpage", "/mainpage/**",
-                                "/signup", "/image/**", "/anicareFile/**",
+                                "/signup", "/image/**", "/anicareFile/**", "/community/**",
                                 "/css/**", "/javascript/**", "/mypage").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
