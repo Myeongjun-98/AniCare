19ee95be1a0e0fd7dc4e7f73c0f61276d30c0c2c
장은호 <bcb119@naver.com>
2025-05-19 15:14:14 +0900

    5.19 찐막

--- src/main/java/com/AniCare/demo/Dto/admin/HospitalDto.java
+++ src/main/java/com/AniCare/demo/Dto/admin/HospitalDto.java
@@ -1,7 +1,7 @@
 package com.AniCare.demo.Dto.admin;
 
-
 import com.AniCare.demo.entity.admin.Hospital;
+import com.AniCare.demo.entity.medical.VetInfo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,26 +26,40 @@ public class HospitalDto {
     private List<String> clinicTypeList; // 다중 선택용 진료 유형
     private String hospitalAddress;
 
+    private List<VetSimpleDto> vetList = new ArrayList<>(); // 수의사 이름 출력용
+
     // 엔티티 → DTO 변환
     public static HospitalDto from(Hospital hospital) {
-        return new HospitalDto(
-                hospital.getId(),
-                hospital.getHospitalName(),
-                hospital.getHospitalTel(),
-                hospital.getHospitalImage(),
+        HospitalDto dto = new HospitalDto();
+
+        dto.setId(hospital.getId());
+        dto.setHospitalName(hospital.getHospitalName());
+        dto.setHospitalTel(hospital.getHospitalTel());
+        dto.setHospitalImage(hospital.getHospitalImage());
+        dto.setDevice(hospital.getDevice());
+        dto.setOperating(hospital.getOperating());
+        dto.setHospitalAddress(hospital.getHospitalAddress());
+
+        dto.setClinicTypeList(
+                hospital.getClinicTypes() != null
+                        ? hospital.getClinicTypes().stream().map(Enum::name).collect(Collectors.toList())
+                        : new ArrayList<>()
+        );
+
+        dto.setVetInfoIdList(
+                hospital.getVetInfos() != null
+                        ? hospital.getVetInfos().stream().map(VetInfo::getId).collect(Collectors.toList())
+                        : new ArrayList<>()
+        );
+
+        dto.setVetList(
                 hospital.getVetInfos() != null
                         ? hospital.getVetInfos().stream()
-                        .map(vet -> vet.getId())
+                        .map(vet -> new VetSimpleDto(vet.getId(), vet.getVetName()))
                         .collect(Collectors.toList())
-                        : new ArrayList<>(),
-                hospital.getDevice(),
-                hospital.getOperating(),
-                hospital.getClinicTypes() != null
-                        ? hospital.getClinicTypes().stream()
-                        .map(Enum::name)
-                        .collect(Collectors.toList())
-                        : new ArrayList<>(),
-                hospital.getHospitalAddress()
+                        : new ArrayList<>()
         );
+
+        return dto;
     }
 }
