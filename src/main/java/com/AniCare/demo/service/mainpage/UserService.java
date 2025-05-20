8da5d570e0a7e5e935129e6b8f6db6a24b3792b6
Myeongjun-98 <anju304@naver.com>
2025-05-20 13:35:42 +0900

    최종수정중

--- src/main/java/com/AniCare/demo/service/mainpage/UserService.java
+++ src/main/java/com/AniCare/demo/service/mainpage/UserService.java
@@ -15,6 +15,7 @@ import com.AniCare.demo.repository.medical.VetRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,6 +24,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -94,7 +97,7 @@ public class UserService implements UserDetailsService {
         return org.springframework.security.core.userdetails.User.builder()
                 .username(username)
                 .password(password)
-                .roles(role)
+                .authorities(Collections.singletonList(new SimpleGrantedAuthority(role)))
                 .build();
     }
 
